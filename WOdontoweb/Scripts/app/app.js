85acb97b4fbbc14d669382b43a9f4cdeaf679cf5
Geovanna Serrano <gserrano@innsa.com>
2016-05-18 08:21:12 -0400

    loader

--- WOdontoweb/Scripts/app/app.js
+++ WOdontoweb/Scripts/app/app.js
@@ -35,11 +35,41 @@ app.config(['$routeProvider',
             redirectTo: 'inicioCliente'
         });
   }]);
-app.factory('injectorDacasys', ['$q', function ($q) {
+
+
+
+
+app.factory('injectorDacasys', ['$rootScope','$q', function ($rootScope, $q) {
+    var numLoadings = 0;
+    $rootScope.countRequest = 0;
     var sessionInjector = {
         request: function (config) {
+            numLoadings++;
+            $rootScope.countRequest++;
             config.url = $("#basePath").attr("href") + config.url;
             return config;
+        },
+        response: function(response) {
+            $rootScope.countRequest--;
+            if ((--numLoadings) === 0) {
+                // Hide loader
+
+                //$rootScope.$broadcast("loader_hide");
+               // $rootScope.lastResponse = 0;
+                //$(document).ready(function() {
+                //    window.setTimeout(function() {
+                //        if ($rootScope.countRequest === 0) {
+                //            //console.log('la ultima');
+                //            var div = $('<div id="continue">');
+                //            //div.css('display', 'inline-block');
+                //            div.css('background', 'cadetblue');
+                //            div.css('width', '1px');
+                //            div.css('height', '1px');
+                //            $('#logormtools').append(div);
+                //        }
+                //    }, 2000);
+                //});
+            }
         }
     };
     return sessionInjector;
@@ -47,3 +77,4 @@ app.factory('injectorDacasys', ['$q', function ($q) {
 app.config(['$httpProvider', function ($httpProvider) {
     $httpProvider.interceptors.push('injectorDacasys');
 }]);
+
