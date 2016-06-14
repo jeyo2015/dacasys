633f4b7a1abee00fa461523356e5a4647526a42a
Brenda <brendavelasco@gmail.com>
2016-06-13 23:54:48 -0400

    update consultorio

--- WOdontoweb/Controllers/EmpresaController.cs
+++ WOdontoweb/Controllers/EmpresaController.cs
@@ -7,39 +7,39 @@
 
     public class EmpresaController : Controller
     {
-        public JsonResult GetAllClinicas()
+        public JsonResult ObtenerClinicas()
         {
-            var result = ABMEmpresa.GetTodasClinicas();
+            var result = ABMEmpresa.ObtenerClinicas();
             return Json(result, JsonRequestBehavior.AllowGet);
         }
 
-        public JsonResult GetConsultorioByID(int pIdConsultorio)
+        public JsonResult ObtenerConsultorioPorId(int idConsultorio)
         {
-            var result = ABMEmpresa.GetConsultorioByID(pIdConsultorio);
+            var result = ABMEmpresa.ObtenerConsultorioPorId(idConsultorio);
             return Json(result, JsonRequestBehavior.AllowGet);
         }
 
-        public JsonResult GetAllClinicasHabilitadas()
+        public JsonResult ObtenerClinicasHabilitadas()
         {
-            var result = ABMEmpresa.GetTodasClinicasHabilitadas();
+            var result = ABMEmpresa.ObtenerClinicasHabilitadas();
             return Json(result, JsonRequestBehavior.AllowGet);
         }
 
-        public JsonResult GetTrabajosClinica(int pIdClinica)
+        public JsonResult ObtenerTrabajosClinica(int idClinica)
         {
-            var result = ABMEmpresa.GetTrabajosClinica(pIdClinica);
-            return Json(result, JsonRequestBehavior.AllowGet);
-        }
-        
-        public JsonResult GetIntervalosTiempo()
-        {
-            var result = ABMEmpresa.Get_Intervalosp();
+            var result = ABMEmpresa.ObtenerTrabajosClinica(idClinica);
             return Json(result, JsonRequestBehavior.AllowGet);
         }
 
-        public JsonResult InsertarNuevaClinica(ClinicaDto pClinica)
+        public JsonResult ObtenerIntervalosDeTiempo()
         {
-            var insert = ABMEmpresa.InsertarClinica( pClinica, Session["loginusuario"].ToString());
+            var result = ABMEmpresa.ObtenerIntervalosDeTiempo();
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult InsertarNuevaClinica(ClinicaDto clinicaDto)
+        {
+            var insert = ABMEmpresa.InsertarClinica( clinicaDto, Session["loginusuario"].ToString());
             var result = new ResponseModel()
             {
                 Message = insert == 1 ? "Se inserto correctamente la clinica" : "No se pudo insertar la clinica, intente de nuevo por favor",
@@ -49,9 +49,9 @@
             return Json(result, JsonRequestBehavior.AllowGet);
         }
 
-        public JsonResult ModificarClinica(ClinicaDto pClinica)
+        public JsonResult ModificarClinica(ClinicaDto clinicaDto)
         {
-            var insert = ABMEmpresa.ModificarClinica(pClinica, Session["loginusuario"].ToString());
+            var insert = ABMEmpresa.ModificarClinica(clinicaDto, Session["loginusuario"].ToString());
             var result = new ResponseModel()
             {
                 Message = insert == 1 ? "Se modifico correctamente la clinica" : "No se pudo modificar la clinica, intente de nuevo por favor",
@@ -61,9 +61,9 @@
             return Json(result, JsonRequestBehavior.AllowGet);
         }
 
-        public JsonResult EliminarClinica(int pIDClinica)
+        public JsonResult EliminarClinica(int idClinica)
         {
-            var insert = ABMEmpresa.EliminarClinica(pIDClinica, Session["loginusuario"].ToString());
+            var insert = ABMEmpresa.EliminarClinica(idClinica, Session["loginusuario"].ToString());
             var result = new ResponseModel()
             {
                 Message = insert == 1 ? "Se deshabilito correctamente la clinica" : "No se pudo deshabilitar la clinica, intente de nuevo por favor",
@@ -73,13 +73,25 @@
             return Json(result, JsonRequestBehavior.AllowGet);
         }
 
-        public JsonResult InsertarNuevoConsultorio(ConsultorioDto pConsultorio) {
-            pConsultorio.IDUsuarioCreador = Session["loginusuario"].ToString();
-            var insert = ABMEmpresa.InsertarConsultorio(pConsultorio);
+        public JsonResult InsertarNuevoConsultorio(ConsultorioDto consultorioDto) {
+            consultorioDto.IDUsuarioCreador = Session["loginusuario"].ToString();
+            var insert = ABMEmpresa.InsertarConsultorio(consultorioDto);
             var result = new ResponseModel()
             {
                 Message = insert == 1 ? "Se inserto correctamente el consultorio" : "No se pudo insertar el consultorio, intente de nuevo por favor",
                 Data = insert
+            };
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ModificarConsultorio(ConsultorioDto consultorioDto)
+        {
+            var insert = ABMEmpresa.ModificarConsultorio(consultorioDto, Session["loginusuario"].ToString());
+            var result = new ResponseModel()
+            {
+                Message = insert == 1 ? "Se modifico correctamente el consultorio" : "No se pudo modificar el consultorio, intente de nuevo por favor",
+                Data = insert,
+                Success = insert == 1
             };
             return Json(result, JsonRequestBehavior.AllowGet);
         }
